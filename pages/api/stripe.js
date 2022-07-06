780e0f65d4173c02f1c9f4578bd699ec647f2f10
PlaksaVlad9 <plaksavla9@gmail.com>
2022-07-06 19:52:42 +0200

    currency change

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         payment_method_types: ["card"],
         billing_address_collection: "auto",
         shipping_options: [
-          { shipping_rate: "shr_1LIZoQLuyhdrAFJnqHdz1wbS" },
+          { shipping_rate: "shr_1LIcNILuyhdrAFJnaOkrfArd" },
           { shipping_rate: "shr_1LIZp4LuyhdrAFJnzBZMDoUu" },
         ],
         line_items: req.body.map((item) => {
